bd3900a3dcdcafbaa42237ea242e4c8d884f1b6c
Bibek Koirala <kbibek371@gmail.com>
2024-01-24 23:33:58 -0500

    rmse function completed

--- rmse.py
+++ rmse.py
@@ -3,5 +3,7 @@ import numpy as np
 def rmse(predictions, targets):
     pred = np.array(predictions)
     tar = np.array(targets)
-    rmse = None # TODO: Implement RMSE Calculation here...
+    mse = np.mean((pred - tar)**2)
+    print("mse1:\n", mse)
+    rmse = np.sqrt(mse) #None # TODO: Implement RMSE Calculation here...
     return rmse
